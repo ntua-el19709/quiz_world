2e208dda20ee6e3ee3a97836b2b45eb8dcd8a8f4
ntua-el19709 <el19709@mail.ntua.gr>
2023-02-25 15:33:04 +0200

    routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:my_app/pages/Category.dart';
+import 'package:my_app/pages/ChallengeMode.dart';
+import 'package:my_app/pages/QuizoftheDay.dart';
+import './pages/widgets/OptionButton.dart';
+import './pages/widgets/Question.dart';
+import './pages/widgets/NavBar.dart';
+import './pages/widgets/NavButton.dart';
+import './pages/HomePage.dart';
+import './pages/Categories.dart';
+import './pages/QuizoftheDayCompleted.dart';
+import './pages/ChallengeCompleted.dart';
+import './pages/QuizCompleted.dart';
+import './pages/QuizPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,13 +38,37 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/': (context) => HomePage(title: 'Flutter Demo'),
+        '/Categories': (context) => Categories(title: 'Flutter Demo'),
+        '/Challenge': (context) => Challenge(title: 'Flutter Demo'),
+        '/QOTD': (context) => QuizoftheDay(title: 'Flutter Demo', days: 10),
+        '/Category': (context) =>
+            Category(title: 'Flutter Demo', category: 'History'),
+        '/QuizCompleted': (context) =>
+            QuizCompleted(title: 'Flutter Demo', score: 90),
+        '/QOTDCompleted': (context) =>
+            QuizoftheDayCompleted(title: 'Flutter Demo', score: 90),
+        '/ChallengeCompleted': (context) =>
+            ChallengeCompleted(title: 'Flutter Demo', score: 90),
+        '/QuizPage': (context) => QuizPage(
+              title: 'Flutter Demo',
+              quiztitle: 'deigma',
+            )
+      },
+      debugShowCheckedModeBanner: false,
+      /*
+      home: const QuizCompleted(
+        title: 'Flutter Demo Home Page',
+        score: 95,
+      ),
+      */
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+/*
+class QuizCompleted extends StatefulWidget {
+  const QuizCompleted({super.key, required this.title, required this.score});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -41,14 +78,24 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
   final String title;
-
+  final int score;
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<QuizCompleted> createState() => _QuizCompletedState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _QuizCompletedState extends State<QuizCompleted> {
+  List<String> quizes = [
+    'WW1',
+    'WW2',
+    'Balkan Wars',
+    'Byzantium',
+    '1821',
+    'Quiz 6',
+    'Quiz 7',
+    'Quiz 8'
+  ];
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -62,6 +109,15 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  List<Widget> getQuizes() {
+    List<Widget> QuizCompletedNames = [];
+    for (int i = 0; i < 8; i++) {
+      QuizCompletedNames.add(NavButton(btext: quizes[i], next: quizes[i]));
+      QuizCompletedNames.add(Container(height: 10));
+    }
+    return QuizCompletedNames;
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -69,11 +125,12 @@ class _MyHomePageState extends State<MyHomePage> {
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // than having to individually change instances of/pages widgets.
     double heightsize = 100;
+    int score = widget.score;
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the QuizCompleted object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
@@ -109,7 +166,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: heightsize,
                   child: Stack(
                     children: <Widget>[
-                      Text('QUIZ WORLD',
+                      Text('Challenge Completed',
                           style: TextStyle(
                             fontSize: 80,
                             foreground: Paint()
@@ -117,7 +174,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               ..strokeWidth = 6
                               ..color = Colors.black,
                           )),
-                      Text('QUIZ WORLD',
+                      Text('Challenge Completed',
                           style: TextStyle(
                             fontSize: 80,
                             color: Colors.white,
@@ -125,27 +182,84 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   )),
               Container(
-                height:
-                    MediaQuery.of(context).size.height - 2 * heightsize - 56,
+                height: MediaQuery.of(context).size.height - heightsize - 520,
               ),
               Container(
+                height: 200,
+                width: 200,
+                child: TextButton(
+                  //autofocus: false,
+                  //clipBehavior: Clip.none,
+                  style: ElevatedButton.styleFrom(
+                      //backgroundColor: Colors.black,
+                      primary: Color.fromARGB(210, 255, 255, 255),
+                      //onPrimary: Colors.white,
+                      //onSurface: Colors.purple,
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                      side: BorderSide(width: 3, color: Colors.black)),
+                  onPressed: () {},
+                  child: Text('SCORE\n\n    $score',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.black,
+                      )),
+                ),
+              ),
+              Container(height: 20),
+              Container(
+                height: 50,
+                width: 500,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 2, color: Colors.black),
+                    ),
+                    hintText: 'user',
+                    labelText: 'Enter your name',
+                    filled: true,
+                    fillColor: Color.fromARGB(210, 255, 255, 255),
+                  ),
+                  controller: TextEditingController(),
+                ),
+              ),
+              Container(height: 20),
+              Container(
                 height: heightsize,
-                child: Text(
-                  'Tap anywhere to continue',
-                  style: Theme.of(context).textTheme.headlineMedium,
+                child: Stack(
+                  children: <Widget>[
+                    Text('Click anywhere to continue',
+                        style: TextStyle(
+                          fontSize: 15,
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 6
+                            ..color = Colors.black,
+                        )),
+                    Text('Click anywhere to continue',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                        )),
+                  ],
                 ),
               ),
             ],
           ),
         ),
       ),
-      /*
-      floatingActionButton: FloatingActionButton(
+      /*floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      */ // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Color.fromARGB(210, 255, 255, 255),
+        child: const Icon(
+          Icons.arrow_back,
+          color: Colors.purple,
+        ),
+      ),*/
     );
   }
 }
+  */
