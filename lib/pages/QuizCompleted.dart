c64a5e9e72b21ae663521efd1f9fd8db93b9976d
ntua-el19709 <el19709@mail.ntua.gr>
2023-02-26 13:06:52 +0200

    gesture QuizCompleted

--- lib/pages/QuizCompleted.dart
+++ lib/pages/QuizCompleted.dart
@@ -68,15 +68,8 @@ class _QuizCompletedState extends State<QuizCompleted> {
 
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _onPressed() {
+    Navigator.pushNamed(context, "/Categories");
   }
 
   List<Widget> getQuizes() {
@@ -99,111 +92,116 @@ class _QuizCompletedState extends State<QuizCompleted> {
     double heightsize = 100;
     int score = widget.score;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the QuizCompleted object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("../background.png"),
-            fit: BoxFit.cover,
-          ),
+        appBar: AppBar(
+          // Here we take the value from the QuizCompleted object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-        child: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
+        body: GestureDetector(
+          onTap: _onPressed,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("../background.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              // Center is a layout widget. It takes a single child and positions it
+              // in the middle of the parent.
+              child: Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Invoke "debug painting" (press "p" in the console, choose the
+                // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                // to see the wireframe for each widget.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
 
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                  height: heightsize,
-                  child: Stack(
-                    children: <Widget>[
-                      Text('Quiz Completed',
-                          style: TextStyle(
-                            fontSize: 80,
-                            foreground: Paint()
-                              ..style = PaintingStyle.stroke
-                              ..strokeWidth = 6
-                              ..color = Colors.black,
-                          )),
-                      Text('Quiz Completed',
-                          style: TextStyle(
-                            fontSize: 80,
-                            color: Colors.white,
-                          )),
-                    ],
-                  )),
-              Container(
-                height: MediaQuery.of(context).size.height - heightsize - 520,
-              ),
-              Container(
-                height: 200,
-                width: 200,
-                child: TextButton(
-                  //autofocus: false,
-                  //clipBehavior: Clip.none,
-                  style: ElevatedButton.styleFrom(
-                      //backgroundColor: Colors.black,
-                      primary: Color.fromARGB(210, 255, 255, 255),
-                      //onPrimary: Colors.white,
-                      //onSurface: Colors.purple,
-                      padding: EdgeInsets.symmetric(horizontal: 16.0),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      ),
-                      side: BorderSide(width: 3, color: Colors.black)),
-                  onPressed: () {},
-                  child: Text('SCORE\n\n    $score',
-                      style: TextStyle(
-                        fontSize: 40,
-                        color: Colors.black,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      height: heightsize,
+                      child: Stack(
+                        children: <Widget>[
+                          Text('Quiz Completed',
+                              style: TextStyle(
+                                fontSize: 80,
+                                foreground: Paint()
+                                  ..style = PaintingStyle.stroke
+                                  ..strokeWidth = 6
+                                  ..color = Colors.black,
+                              )),
+                          Text('Quiz Completed',
+                              style: TextStyle(
+                                fontSize: 80,
+                                color: Colors.white,
+                              )),
+                        ],
                       )),
-                ),
+                  Container(
+                    height:
+                        MediaQuery.of(context).size.height - heightsize - 520,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    child: TextButton(
+                      //autofocus: false,
+                      //clipBehavior: Clip.none,
+                      style: ElevatedButton.styleFrom(
+                          //backgroundColor: Colors.black,
+                          primary: Color.fromARGB(210, 255, 255, 255),
+                          //onPrimary: Colors.white,
+                          //onSurface: Colors.purple,
+                          padding: EdgeInsets.symmetric(horizontal: 16.0),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20.0)),
+                          ),
+                          side: BorderSide(width: 3, color: Colors.black)),
+                      onPressed: () {},
+                      child: Text('SCORE\n\n    $score',
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.black,
+                          )),
+                    ),
+                  ),
+                  Container(height: 150),
+                  Container(
+                    height: heightsize,
+                    child: Stack(
+                      children: <Widget>[
+                        Text('Click anywhere to continue',
+                            style: TextStyle(
+                              fontSize: 15,
+                              foreground: Paint()
+                                ..style = PaintingStyle.stroke
+                                ..strokeWidth = 6
+                                ..color = Colors.black,
+                            )),
+                        Text('Click anywhere to continue',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            )),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Container(height: 150),
-              Container(
-                height: heightsize,
-                child: Stack(
-                  children: <Widget>[
-                    Text('Click anywhere to continue',
-                        style: TextStyle(
-                          fontSize: 15,
-                          foreground: Paint()
-                            ..style = PaintingStyle.stroke
-                            ..strokeWidth = 6
-                            ..color = Colors.black,
-                        )),
-                    Text('Click anywhere to continue',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
-                  ],
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-      /*floatingActionButton: FloatingActionButton(
+        )
+        /*floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         backgroundColor: Color.fromARGB(210, 255, 255, 255),
@@ -212,6 +210,6 @@ class _QuizCompletedState extends State<QuizCompleted> {
           color: Colors.purple,
         ),
       ),*/
-    );
+        );
   }
 }
